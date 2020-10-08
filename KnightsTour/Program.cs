95fbe9fe2979939431ce31357b3a686b350010e7
ketiovv <wojtek.kajstura@gmail.com>
2020-10-08 14:20:27 +0200

    refactored program.cs

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -27,24 +27,7 @@ namespace KnightsTour
                 if (knight.SquaresVisited.Count == 64)
                 {
                     allSquaresVisited = true;
-
-                    Console.WriteLine("----");
-
-                    Console.WriteLine($"Actual position: {knight.ActualSquare}");
-
-                    Console.WriteLine($"Possible moves: ");
-                    foreach (var move in knight.PossibleMoves)
-                    {
-                        Console.WriteLine(move);
-                    }
-
-                    Console.WriteLine($"Visited squares: ");
-                    for (var i = 0; i < knight.SquaresVisited.Count; i++)
-                    {
-                        Console.WriteLine($"{i + 1}. {knight.SquaresVisited[i]}");
-                    }
-
-                    Console.WriteLine("----");
+                    knight.PrintPath();
                 }
             }
         }
