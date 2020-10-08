c9756d4a3edefb0603ccadc2a5dbbd25badfc7e5
ketiovv <wojtek.kajstura@gmail.com>
2020-10-08 12:59:16 +0200

    tested created classes in main

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -6,7 +6,16 @@ namespace KnightsTour
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var chessboard = new Chessboard(8);
+            var knight = new Knight(4, 4, chessboard.Size);
+
+            Console.WriteLine($"Actual position: {knight.ActualSquare}");
+            Console.WriteLine($"Possible moves: ");
+            foreach (var move in knight.PossibleMoves)
+            {
+                Console.WriteLine(move);
+            }
+
         }
     }
 }
