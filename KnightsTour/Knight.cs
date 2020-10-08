1e9c7702ff28b7c44ce4c827245aedd421c411b1
ketiovv <wojtek.kajstura@gmail.com>
2020-10-08 12:58:31 +0200

    created knight class

---
+++ KnightsTour/Knight.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KnightsTour
+{
+    public class Knight
+    {
+        public Square ActualSquare { get; set; }
+        public List<Square> PossibleMoves { get; set; } = new List<Square>();
+        public List<Square> SquaresVisited { get; set; } = new List<Square>();
+
+        public Knight(int startingX, int startingY, int chessboardSize)
+        {
+            ActualSquare = new Square(startingX, startingY);
+            PossibleMoves = GeneratePossibleMoves(ActualSquare, chessboardSize);
+        }
+
+        public List<Square> GeneratePossibleMoves(Square actualSquare, int chessboardSize)
+        {
+            var possibleMoves = new List<Square>
+            {
+                new Square(actualSquare.X + 2, actualSquare.Y + 1),
+                new Square(actualSquare.X + 2, actualSquare.Y - 1),
+
+                new Square(actualSquare.X - 2, actualSquare.Y + 1),
+                new Square(actualSquare.X - 2, actualSquare.Y - 1),
+
+                new Square(actualSquare.X + 1, actualSquare.Y + 2),
+                new Square(actualSquare.X - 1, actualSquare.Y + 2),
+
+                new Square(actualSquare.X + 1, actualSquare.Y - 2),
+                new Square(actualSquare.X - 1, actualSquare.Y - 2)
+            };
+            var impossibleMoves =
+                possibleMoves.Where(square =>
+                                        square.X < 0 || square.X > chessboardSize ||
+                                        square.Y < 0 || square.Y > chessboardSize).ToList();
+
+
+            foreach (var square in impossibleMoves)
+            {
+                possibleMoves.Remove(square);
+            }
+
+
+            return possibleMoves;
+        }
+    }
+}
