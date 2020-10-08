84920ecb88205e5b268f3702d9c367008393614d
ketiovv <wojtek.kajstura@gmail.com>
2020-10-08 12:34:41 +0200

    created chessboard class

---
+++ KnightsTour/Chessboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnightsTour
+{
+    public class Chessboard
+    {
+        public int Size { get; set; }
+        public IEnumerable<Square> AllSquares { get; set; }
+
+        public Chessboard(int size)
+        {
+            Size = size;
+            AllSquares = GenerateChessboard(size);
+        }
+
+        public IEnumerable<Square> GenerateChessboard(int size)
+        {
+            var allSquaresOnChessboard = new List<Square>();
+
+            for (var i = 1; i <= size; i++)
+            {
+                for (var j = 1; j <= size; j++)
+                {
+                    allSquaresOnChessboard.Add(new Square(i, j));
+                }
+            }
+
+            return allSquaresOnChessboard;
+        }
+    }
+}
