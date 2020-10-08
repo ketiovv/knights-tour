dd8260840548e776d5ce46d4597ef39d8ff11ee2
ketiovv <wojtek.kajstura@gmail.com>
2020-10-08 12:34:32 +0200

    created square class

---
+++ KnightsTour/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnightsTour
+{
+    public class Square
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Square(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+}
