a3761302f8d3d1cc78c03e8d06b0e50ad3be5176
Wojtek <wojtek.kajstura@gmail.com>
2020-11-13 19:28:25 +0100

    refactored

--- KnightsTour/Program.cs
+++ KnightsTour/Program.cs
@@ -6,7 +6,7 @@ namespace KnightsTour
     {
         static void Main(string[] args)
         {
-            FindKnightsTourByBruteForce(5, 0, 0);
+            FindKnightsTourByBruteForce(6, 0, 0);
         }
 
         private static void FindKnightsTourByBruteForce(int chessboardSize, int startingX, int startingY)
@@ -19,39 +19,21 @@ namespace KnightsTour
                 attempts++;
                 Console.WriteLine(attempts);
                 var rnd = new Random();
-                var chessboard = new Chessboard(5);
-                var knight = new Knight(0, 0, chessboard.Size);
-                var i = 1;
+                var chessboard = new Chessboard(chessboardSize);
+                var knight = new Knight(startingX, startingY, chessboardSize);
 
-                //Console.WriteLine($"{i} - starting from {knight.ActualSquare.ToString()}");
                 do
                 {
-                    i++;
-                    //Console.WriteLine($"---------------------------------------------------");
-
-                    //Console.WriteLine($"{i} - list of possible moves:");
-                    //foreach (var moves in knight.PossibleMoves)
-                    //{
-                    //    Console.WriteLine($"{moves}");
-                    //}
-
                     if (knight.PossibleMoves.Count > 0)
                     {
                         var randomMove = rnd.Next(0, knight.PossibleMoves.Count - 1);
                         var selectedMove = knight.PossibleMoves[randomMove];
                         knight.ActualSquare = selectedMove;
-                        //Console.WriteLine($"Selected {selectedMove}");
-                        //Console.WriteLine($"possible moves: {knight.PossibleMoves.Count}");
-                        //Console.WriteLine($"from {0} to {knight.PossibleMoves.Count - 1} we've got move with index {randomMove} in possible moves");
                     }
-                    else
-                    {
-                        //Console.WriteLine("You can't do more moves");
-                    }
-
-                    //Console.WriteLine($"---------------------------------------------------");
                 } while (knight.PossibleMoves.Count > 0);
+
                 knight.SquaresVisited.Add(knight.ActualSquare);
+
                 if (knight.SquaresVisited.Count == chessboard.Size * chessboard.Size)
                 {
                     winner = true;
